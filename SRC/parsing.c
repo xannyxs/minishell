f445dca14d1996897975fdb2e2f6021d263cc3b0
Dikkexander <xandervoorvaart@protonmail.com>
2022-02-16 14:24:34 +0100

    Started the parsing

--- SRC/parsing.c
+++ SRC/parsing.c
@@ -6,7 +6,7 @@
 /*   By: xvoorvaa <xvoorvaa@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/16 14:15:14 by xvoorvaa      #+#    #+#                 */
-/*   Updated: 2022/02/16 14:20:18 by xvoorvaa      ########   odam.nl         */
+/*   Updated: 2022/02/16 14:22:49 by xvoorvaa      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 #include <stdio.h>
 
-int start_parsing(char **prompt)
+int	start_parsing(char **prompt)
 {
-    printf("%s\n", prompt[0]);
-    return (0);
+	printf("%s\n", prompt[0]);
+	return (0);
 }
