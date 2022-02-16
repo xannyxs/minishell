40ab7ba4a48da0dfc3002ab08c95b135592fc14c
Job van der Zweep <jobvan-d@student.codam.nl>
2022-02-16 13:26:17 +0100

    big brain moment

--- SRC/minishell.c
+++ SRC/minishell.c
@@ -6,25 +6,35 @@
 /*   By: xander <xander@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/14 18:11:55 by xander        #+#    #+#                 */
-/*   Updated: 2022/02/16 12:56:14 by jobvan-d      ########   odam.nl         */
+/*   Updated: 2022/02/16 13:26:12 by jobvan-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
-int main(void)
+int	main(void)
 {
 	char	*str;
 
 	str = (char *)342;
-	while (str)
+	while (1)
 	{
-		str = readline("hallo?");
-		printf("%s", str);
+		str = readline("minishell $> ");
+		if (str == NULL)
+		{
+			printf("exit\n");
+			exit(0);
+		}
+		else
+		{
+			printf("[%s]\n", str);
+			free(str);
+		}
 	}
 	return (0);
 }
