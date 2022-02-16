e7ab5d1d199c1a788018c76e32a0f155af376c0b
Dikkexander <xandervoorvaart@protonmail.com>
2022-02-16 16:27:03 +0100

    Added a linked list with all the info for the exec

--- INC/minishell.h
+++ INC/minishell.h
@@ -6,7 +6,7 @@
 /*   By: xvoorvaa <xvoorvaa@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/01 14:09:18 by xvoorvaa      #+#    #+#                 */
-/*   Updated: 2022/02/16 16:08:53 by jobvan-d      ########   odam.nl         */
+/*   Updated: 2022/02/16 16:25:17 by xvoorvaa      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,18 @@
 # include <errno.h>
 # include "libft.h"
 
-enum e_token { T_LITERAL, T_PIPE, T_REDIRECT, T_REDIRECT_FILE_TO_STDIN };
+enum e_token {
+	T_LITERAL,
+	T_PIPE,
+	T_REDIRECT,
+	T_REDIRECT_FILE_TO_STDIN
+};
 
 typedef struct s_token
 {
-	char			**content;
-	struct s_token	*next;
+	char			*content;
 	enum e_token	*token;
+	struct s_token	*next;
 }			t_token;
 
 typedef struct s_parsing
@@ -30,14 +35,22 @@ typedef struct s_parsing
 	char	**prompt;
 	int		flag_check;
 	int		pipe_check;
-	char	**commands;
-	char	**text;
+	t_token	*tokenizer;
 }	t_parsing;
 
 /*
 	MINISHELL
 */
 
-int		start_parsing(char **prompt, t_parsing *parsing);
+int		init_parsing(t_parsing *parsing);
+
+
+/*
+	LINKED LIST
+*/
+
+void	new_node(t_token **head, char *content);
+
+void	print_list(t_token *head);
 
 #endif
