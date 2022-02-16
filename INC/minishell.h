5a11665d77c0aae171b404239ecde398c8728bc3
Job van der Zweep <jobvan-d@student.codam.nl>
2022-02-16 16:09:16 +0100

    struct linked list

--- INC/minishell.h
+++ INC/minishell.h
@@ -6,7 +6,7 @@
 /*   By: xvoorvaa <xvoorvaa@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/01 14:09:18 by xvoorvaa      #+#    #+#                 */
-/*   Updated: 2022/02/16 15:54:22 by xvoorvaa      ########   odam.nl         */
+/*   Updated: 2022/02/16 16:08:53 by jobvan-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,15 @@
 
 # include <errno.h>
 # include "libft.h"
+
+enum e_token { T_LITERAL, T_PIPE, T_REDIRECT, T_REDIRECT_FILE_TO_STDIN };
+
+typedef struct s_token
+{
+	char			**content;
+	struct s_token	*next;
+	enum e_token	*token;
+}			t_token;
 
 typedef struct s_parsing
 {
