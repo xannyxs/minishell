b7b36c93bf142790659792bc26f6bd3e18c2d6e3
Dikkexander <xandervoorvaart@protonmail.com>
2022-02-23 18:11:24 +0100

    Got rid of the pipe()

--- SRC/exec_command.c
+++ SRC/exec_command.c
@@ -6,7 +6,7 @@
 /*   By: xvoorvaa <xvoorvaa@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/23 16:55:19 by xvoorvaa      #+#    #+#                 */
-/*   Updated: 2022/02/23 17:54:31 by xvoorvaa      ########   odam.nl         */
+/*   Updated: 2022/02/23 18:09:51 by xvoorvaa      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,24 +51,17 @@ static char	**allocate_input(t_vars *vars)
 
 int	exec_command(t_vars *vars)
 {
-	int		fd[2];
 	pid_t	fork_id1;
 	char	*usr_func;
 	char	**input;
 
-	if (pipe(fd) < 0)
-		return (-1);
 	fork_id1 = fork();
 	if (fork_id1 == 0)
 	{
-		if (close(fd[READ]) < 0)
-			return (-1);
 		input = allocate_input(vars);
 		usr_func = path_check(vars->token_list->content, \
 			find_dir(vars->environ));
 		if (usr_func == NULL || input == NULL)
-			return (-1);
-		if (close(fd[WRITE]) < 0)
 			return (-1);
 		execve(usr_func, input, vars->environ);
 		ft_putstr_fd("ERROR\n", STDERR_FILENO);
@@ -76,7 +69,6 @@ int	exec_command(t_vars *vars)
 	}
 	if (fork_id1 < 0)
 		return (-1);
-	if (close(fd[READ]) < 0 || close(fd[WRITE]) < 0)
-		return (-1);
+	waitpid(fork_id1, NULL, 0);
 	return (0);
 }
